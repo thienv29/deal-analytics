2560a7a84ab563115ffcc82107f14d42a7e0a221
thienv29 <thienv29@gmail.com>
2025-10-03 16:04:03 +0700

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -16,22 +16,17 @@ export function toTitleCase(raw?: string): string {
     .join(" ");
 }
 
-export function normalizeVietnamPhone(raw: string): string {
+export function normalizeVietnamPhone(raw?: string): string {
   if (!raw) return "";
   
-  // 1. Bỏ khoảng trắng, dấu . hoặc -
   let phone = raw.replace(/[\s\-\.\(\)]/g, "");
 
-  // 2. Nếu bắt đầu bằng +84 => đổi thành 0
   if (phone.startsWith("+84")) {
     phone = "0" + phone.slice(3);
   } else if (phone.startsWith("84")) {
-    // hoặc 84xxxxxx
     phone = "0" + phone.slice(2);
   }else if (!phone.startsWith("0")) {
-    // nếu không có 0 đầu thì tự thêm
     phone = "0" + phone;
   }
-  // 3. Nếu đã bắt đầu bằng 0 thì giữ nguyên
   return phone;
 }
